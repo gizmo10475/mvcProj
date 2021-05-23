3f57d1ec7264cfc0dcb6780bcb2b9de7b8ebe9bd
Edvin Fajkovic <edvin.fajkovic3@gmail.com>
2021-05-23 21:14:10 +0100

    first commit

---
+++ tests/DiceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dice;
+
+use PHPUnit\Framework\TestCase;
+
+// use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Testing Dice class.
+ */
+class DiceTest extends TestCase
+{
+    /**
+     * Checking roll and getLastRoll.
+     */
+    public function testRoll()
+    {
+        $dice = new Dice();
+
+        $res = $dice->roll();
+        $exp = $dice->getLastRoll();
+        $this->assertSame($exp, $res);
+    }
+}

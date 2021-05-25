63a9e37c3e43af588d269779bf0dd994d5b483e5
Edvin Fajkovic <edvin.fajkovic3@gmail.com>
2021-05-25 01:22:47 +0100

    dice1 done

--- src/Controller/Game21Controller.php
+++ src/Controller/Game21Controller.php
@@ -18,6 +18,10 @@ class Game21Controller extends AbstractController
      */
     public function start(SessionInterface $session, Request $request): Response
     {
+        if ($session->get('coinsUsr') == null) {
+            $session->set('coinsUsr', 10);
+            $session->set('coinsBot', 100);
+        }
         $session->set('scoreUsr', 0);
         $session->set('scoreBot', 0);
         $session->set('stopgame', 0);
@@ -25,12 +29,28 @@ class Game21Controller extends AbstractController
         $session->set('dice2graph', 0);
         $session->set('totalscore', 0);
         $session->set('dicelist', []);
+        $session->set('coinbetUsr', 0);
+        $session->get('coinsUsr');
+        $session->get('coinsBot');
+        $halfCoins = $session->get('coinsUsr') / 2;
+        $session->set('halfCoins', $halfCoins);
 
 
+        if ($request->get('diceChoice') == 'en tärning') {
+            if ($request->get('coinBet')) {
+                $session->set('coinbetUsr', (int)$request->get('coinBet'));
+                $session->set('coinsUsr', $session->get('coinsUsr') - (int)$request->get('coinBet'));
+            }
 
-        return $this->render('game21.html.twig', [
-        ]);
+            return $this->redirectToRoute('dice1');
+        }
+        if ($request->get('diceChoice') == 'två tärningar') {
+            return $this->redirectToRoute('dice2');
+        }
+
+        return $this->render('game21.html.twig');
     }
+
 
     /**
      * @Route("/dice1", name="dice1")
@@ -38,9 +58,11 @@ class Game21Controller extends AbstractController
     public function startgame(SessionInterface $session, Request $request): Response
     {
         $dice = new \App\Dice\DiceGraphic();
-        // $rolls = 1;
         $res = [];
-        // $class = [];
+
+
+        var_dump($session->get('coinbetUsr'));
+
 
         if ($request->get('btn') == 'stop') {
             $session->set('stopgame', 1);
@@ -57,31 +79,28 @@ class Game21Controller extends AbstractController
 
         $session->set('dice1status', false);
 
-
-        // if ($request = $this->get('request')) {
-        //     var_dump("hejkasdiajsdioujahsdkjashdkajhsbnd");
-        // }
         if ($session->get('stopgame') == 1) {
             $session->set('stopgame', 1);
             while ($session->get('scoreBot') < 21) {
                 $bot = $dice->roll();
                 $session->set('scoreBot', ($session->get('scoreBot') + $bot));
+                if ($session->get('scoreBot') == 21) {
+                    
+                }
             }
         }
-
-        var_dump($session->get('scoreBot'));
-
-        // if ($request->get('stop')) {
-        //     while ($session->get('scoreBot') < 21) {
-        //         $bot = $dice->roll();
-        //         $session->set('scoreBot', ($session->get('scoreBot') + $die));
-        //     }
-        // }
-
 
         return $this->render('dice1.html.twig', []);
     }
 
+    /**
+     * @Route("/config")
+     */
+    public function config(SessionInterface $session, Request $request): Response
+    {
+        $session->set('coinsUsr', 10);
+        $session->set('coinsBot', 100);
 
-
+        return $this->render('game21.html.twig');
+    }
 }

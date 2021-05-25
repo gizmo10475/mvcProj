292caa86e825cb34c6c5d6539000bc4d3b9dc117
Edvin Fajkovic <edvin.fajkovic3@gmail.com>
2021-05-25 01:47:38 +0100

    added coinbetting

--- src/Controller/Game21Controller.php
+++ src/Controller/Game21Controller.php
@@ -41,10 +41,13 @@ class Game21Controller extends AbstractController
                 $session->set('coinbetUsr', (int)$request->get('coinBet'));
                 $session->set('coinsUsr', $session->get('coinsUsr') - (int)$request->get('coinBet'));
             }
-
             return $this->redirectToRoute('dice1');
         }
         if ($request->get('diceChoice') == 'två tärningar') {
+            if ($request->get('coinBet')) {
+                $session->set('coinbetUsr', (int)$request->get('coinBet'));
+                $session->set('coinsUsr', $session->get('coinsUsr') - (int)$request->get('coinBet'));
+            }
             return $this->redirectToRoute('dice2');
         }
 
@@ -85,13 +88,64 @@ class Game21Controller extends AbstractController
                 $bot = $dice->roll();
                 $session->set('scoreBot', ($session->get('scoreBot') + $bot));
                 if ($session->get('scoreBot') == 21) {
-                    
+                    $session->set('coinsBot', $session->get('coinsBot') + $session->get('coinbetUsr'));
+                }
+                if ($session->get('scoreBot') > 21) {
+                    $session->set('coinsUsr', $session->get('coinsUsr') + ($session->get('coinbetUsr') * 2));
+                    $session->set('coinsBot', $session->get('coinsBot') - $session->get('coinbetUsr'));
                 }
             }
         }
 
         return $this->render('dice1.html.twig', []);
     }
+
+
+    /**
+     * @Route("/dice2", name="dice2")
+     */
+    public function startgame2(SessionInterface $session, Request $request): Response
+    {
+        $dice = new \App\Dice\DiceGraphic();
+        $res = [];
+
+
+        var_dump($session->get('coinbetUsr'));
+
+
+        if ($request->get('btn') == 'stop') {
+            $session->set('stopgame', 1);
+        }
+
+        if ($session->get('stopgame') == 0) {
+            $die = $dice->rollTwo();
+            $res[] = $die;
+            $session->set('scoreUsr', ($session->get('scoreUsr') + $die));
+            $session->set('dice1graph', $dice->graphic());
+        }
+        $session->set('listan', []);
+        $session->set('listan', [$session->get('dice1graph')]);
+
+        $session->set('dice1status', false);
+
+        if ($session->get('stopgame') == 1) {
+            $session->set('stopgame', 1);
+            while ($session->get('scoreBot') < 21) {
+                $bot = $dice->roll();
+                $session->set('scoreBot', ($session->get('scoreBot') + $bot));
+                if ($session->get('scoreBot') == 21) {
+                    $session->set('coinsBot', $session->get('coinsBot') + $session->get('coinbetUsr'));
+                }
+                if ($session->get('scoreBot') > 21) {
+                    $session->set('coinsUsr', $session->get('coinsUsr') + ($session->get('coinbetUsr') * 2));
+                    $session->set('coinsBot', $session->get('coinsBot') - $session->get('coinbetUsr'));
+                }
+            }
+        }
+
+        return $this->render('dice2.html.twig', []);
+    }
+
 
     /**
      * @Route("/config")
